c31c120ccc8ffce27d7d2ff0129b3901f3582fd4
ApundaTheeShort <apundawyclife333@gmail.com>
2024-03-02 15:18:45 +0300

    added user_input.py

---
+++ user_input.py
@@ -0,0 +1,5 @@
+name= input('Enter your name: ')
+age = input('Enter your age: ')
+location = input('Enter your location: ')
+
+print('hello {}, you are {} years old and live in {}' .format(name, age, location))
