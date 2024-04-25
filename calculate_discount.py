b4b6e3b619a8270778cbb9f243a98f3b4f42ec1a
ApundaTheeShort <apundawyclife333@gmail.com>
2024-04-25 12:15:05 +0300

    "pushing calculate_discount.py"

---
+++ calculate_discount.py
@@ -0,0 +1,16 @@
+
+
+def calculate_discount(price, discount_percent):
+  if discount_percent >= 20:
+    discount = price*(discount_percent*0.01)
+    new_price = price - discount
+    print("The new price is: ", new_price)
+    # return new_price
+  else:
+    new_price = price
+    print("The new price is: ", new_price)
+    
+price = float(input("Enter the Original price: "))
+discount_percent = float(input("Enter the Discount: "))
+final_price = calculate_discount(price, discount_percent)
+# print(f"This is the price: {final_price}")
